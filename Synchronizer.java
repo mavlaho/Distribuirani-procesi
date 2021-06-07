59a2df9f47d604daade4d47c4bca0f3c1c9d2b9a
TinStefanic <48285574+TinStefanic@users.noreply.github.com>
2021-06-07 15:11:23 +0200

    Delete Synchronizer.java

--- Synchronizer.java
+++
@@ -1,5 +0,0 @@
-public interface Synchronizer extends MsgHandler {
-	public void initialize(MsgHandler initProg);
-    	public void sendMessage(int destId, String tag, int msg);
-	public void nextPulse();// block for the next pulse 
-}
