f135daaac98a1b7e60bd26d79c0040b8864a48e7
TinStefanic <tin.stefanic@gmail.com>
2021-06-07 13:57:06 +0200

    Dodan potreban kod s predavanja.

---
+++ MsgHandler.java
@@ -0,0 +1,8 @@
+// Kod s predavanja.
+
+import java.io.*;
+
+public interface MsgHandler {
+    public void handleMsg(Msg m, int srcId, String tag);
+    public Msg receiveMsg(int fromId) throws IOException;
+}
